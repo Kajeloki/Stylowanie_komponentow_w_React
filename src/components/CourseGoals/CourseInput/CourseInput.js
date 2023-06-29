03072c1aa582f138bbfd338d8c53c2c695fa7089
Kajeloki <piotrkw200@wp.pl>
2023-06-29 16:35:19 +0200

    dynamiczne style css w oknie dodania zadania

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,21 +5,26 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
-
+const [isValid, setIsValid]=useState(true);
   const goalInputChangeHandler = event => {
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if(enteredValue.trim().length===0)
+    {
+      setIsValid(false);
+      return;
+    } else{setIsValid(true)}
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{color:!isValid ? "red":"black"}}>Course Goal</label>
+        <input type="text" style={{backgroundColor: !isValid ? "#ffabab":"#fad0ec", border: !isValid ? '1px solid red': '1px solid #ccc'}} onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
